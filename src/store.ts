a8f5d57e672b1246800fc281c461d28fc483fff6
alenaksu <misterroot@gmail.com>
2024-06-02 18:00:47 +0000

    chore: docs

--- src/store.ts
+++ src/store.ts
@@ -11,9 +11,17 @@ export interface Store<S extends object> {
 }
 
 export const createStore = <S extends object>(initialState: S): Store<S> => {
+  /**
+   * The state object is sealed to prevent modification of the state
+   */
   const rawState = Object.seal(structuredClone(initialState));
+  /**
+   * The dependencies map keeps track of which properties are read by each subscription
+   */
   const dependencies = new Map<UpdateHandler, Set<string>>();
-  const pendingPropertyUpdates = new Set<string>();
+  /**
+   * The subscriptions map keeps track of the subscriptions and their associated handlers and revokers
+   */
   const subscriptions = new WeakMap<
     S,
     {
@@ -21,12 +29,23 @@ export const createStore = <S extends object>(initialState: S): Store<S> => {
       revoke: () => void;
     }
   >();
+
+  /**
+   * Keeps track of which properties have been updated since the last update
+   */
+  const pendingPropertyUpdates = new Set<string>();
   let isUpdatePending = false;
 
+  /**
+   * Called whenever a property is written to the state
+   */
   const onWrite = (propertyPath: string) => {
     scheduleUpdate(propertyPath);
   };
 
+  /**
+   * Schedules an update to be run in the next microtask. If an update is already pending, it will be skipped.
+   */
   const scheduleUpdate = async (propertyPath: string) => {
     pendingPropertyUpdates.add(propertyPath);
 
@@ -52,12 +71,18 @@ export const createStore = <S extends object>(initialState: S): Store<S> => {
     });
   };
 
+  /**
+   * Create a public state object that can be read from and written to
+   */
   const state = createStateProxy(rawState, {
     onWrite,
   });
 
   return {
     state,
+    /**
+     * Returns a state object where every read operation is tracked and triggers an update when a property is written to
+     */
     subscribe(handler: UpdateHandler) {
       dependencies.set(handler, new Set());
 
@@ -74,6 +99,9 @@ export const createStore = <S extends object>(initialState: S): Store<S> => {
 
       return proxy;
     },
+    /**
+     * Unsubscribes from a state object updates. The state object will no longer be tracked for updates.
+     */
     unsubscribe(state: S) {
       const { handler, revoke } = subscriptions.get(state)!;
 
